0a78c6d2a566bdf80c59c00a3720583032de72e4
jmavandi <jonathan.mavandi@gmail.com>
2024-11-11 13:31:53 -0500

    add update/delete

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,9 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from "@nestjs/common";
 
-@Controller('cards')
+@Controller("cards")
 export class CardsController {
   @Get()
-  findAll() {
-    return 'this action returns all the cards';
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all cards Limit: ${limit}, offset: ${offset}`;
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return `This action returns ${id} card`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
+    // return `this action creates a card`;
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() body) {
+    return `This actions updates #${id} card`;
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return `This action removes #${id} card`;
   }
 }
